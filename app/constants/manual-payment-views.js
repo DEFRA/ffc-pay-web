00e8ab97284df0cbab544e1ffe14fa31df83a817
Oliver Lewington <oliver.lewington@eviden.com>
2025-08-15 13:03:03 +0100

    Fixing errors and removing redirect option

--- app/constants/manual-payment-views.js
+++ app/constants/manual-payment-views.js
@@ -1,5 +1,5 @@
 module.exports = {
   MANUAL_PAYMENTS: 'manual-payments',
-  UPLOAD: 'manual-payments/upload',
-  LOADING: 'manual-payments/loading'
+  UPLOAD: 'manual-payments/manual-payments-upload',
+  LOADING: 'manual-payments/manual-payments-loading'
 }
--- app/routes/schemas/manual-payment-file-schema.js
+++ app/routes/schemas/manual-payment-file-schema.js
@@ -1,24 +1,31 @@
 const Joi = require('joi')
 
-const filenameRegex = /^FFC_Manual_Batch_.*csv$/
+// Regex to match filenames like: FFC_Manual_Batch_SFI23_20250626091445.csv
+const filenameRegex = /^FFC_Manual_Batch_SFI(?:\d{0,2})?_\d{14}\.csv$/i
 
 module.exports = Joi.object({
-  file: Joi.object().keys({
+  file: Joi.object({
     filename: Joi.string().pattern(filenameRegex).required(),
     path: Joi.string().required(),
-    headers: Joi.object().keys({
+    headers: Joi.object({
       'content-disposition': Joi.string().required(),
       'content-type': Joi.string().valid('text/csv').required()
     }).required(),
     bytes: Joi.number().required()
   }).error(errors => {
-    const err = errors[0]
-    if (err && err.context && err.context.key === 'filename') {
-      err.message = 'Filename must match FFC_Manual_Batch_SFI[_|##]_<timestamp> (e.g. FFC_Manual_Batch_SFI23_20250626091445)'
-    } else {
-      console.error('Manual payment file schema error:', err)
-      err.message = 'Provide a CSV file'
-    }
-    return err
+    errors.forEach(err => {
+      // err.path = ['file', 'filename'] or ['file', 'headers', 'content-type']
+      const key = err.path && err.path[1] // child key inside file
+      console.log('Error key:', key) // Debugging line to check the key
+      if (key === 'filename') {
+        err.message = 'Filename must match FFC_Manual_Batch_SFI_<timestamp> (e.g. FFC_Manual_Batch_SFI23_20250626091445)'
+      } else if (key === 'headers') {
+        err.message = 'File must be a CSV (content-type: text/csv)'
+      } else {
+        err.message = 'Provide a CSV file'
+      }
+    })
+
+    return errors
   })
 })
