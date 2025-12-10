e23bf4bdc87553c285eedbb8ffd0506c8c9f84ff
Sam Plackett <samuel.plackett@eviden.com>
2025-12-10 15:14:03 +0000

    rename reports

--- app/helpers/get-report-types.js
+++ app/helpers/get-report-types.js
@@ -4,12 +4,11 @@ const getReportTypes = () => {
   console.log(`Legacy reports are ${process.env.LEGACY_REPORTS_ACTIVE ? '' : 'not '}active in this environment.`)
   if (config.legacyReportsActive) {
     return {
-      'Payment request statuses - old version to be decommissioned': 'payment-requests',
       'Payment request statuses': 'payment-requests-v2',
       'Suppressed payment requests': 'suppressed-payments',
       'AP-AR listing report': 'ap-ar-report',
       Holds: 'holds',
-      'Request Editor report - received and released from RE dates to be added 08/12/2025': 'request-editor-report',
+      'Request Editor report': 'request-editor-report',
       'Payment statement status report': 'status-report'
     }
   }
