9134695cca52860da09ce02999772fb9f8dce990
John Barnard <john.barnard.external@eviden.com>
2025-12-23 10:28:02 +0000

    removed legacy report active flag
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/helpers/get-report-types.js
+++ app/helpers/get-report-types.js
@@ -1,17 +1,6 @@
 const config = require('../config')
 
 const getReportTypes = () => {
-  console.log(`Legacy reports are ${process.env.LEGACY_REPORTS_ACTIVE ? '' : 'not '}active in this environment.`)
-  if (config.legacyReportsActive) {
-    return {
-      'Payment request statuses': 'payment-requests-v2',
-      'Suppressed payment requests': 'suppressed-payments',
-      'AP-AR listing report': 'ap-ar-report',
-      Holds: 'holds',
-      'Request Editor report': 'request-editor-report',
-      'Payment statement status report': 'status-report'
-    }
-  }
   return {
     // These don't call call tracking api (Report already exists in BLOB)
     'Payment request statuses': 'payment-requests',
--- test/unit/helpers/get-report-types.test.js
+++ test/unit/helpers/get-report-types.test.js
@@ -16,24 +16,7 @@ describe('getReportTypes', () => {
     delete process.env.LEGACY_REPORTS_ACTIVE
   })
 
-  test('should return all report types when legacyReportsActive is true', () => {
-    config.legacyReportsActive = true
-
-    const reportTypes = getReportTypes()
-
-    expect(reportTypes).toEqual({
-      'Payment request statuses': 'payment-requests-v2',
-      'Suppressed payment requests': 'suppressed-payments',
-      'AP-AR listing report': 'ap-ar-report',
-      Holds: 'holds',
-      'Request Editor report': 'request-editor-report',
-      'Payment statement status report': 'status-report'
-    })
-  })
-
-  test('should return limited report types when legacyReportsActive is false', () => {
-    config.legacyReportsActive = false
-
+  test('should return report types', () => {
     const reportTypes = getReportTypes()
 
     expect(reportTypes).toEqual({
@@ -43,19 +26,5 @@ describe('getReportTypes', () => {
       'Payment statement status report': 'status-report'
 
     })
-  })
-
-  test('logs "Legacy reports are active in this environment." when LEGACY_REPORTS_ACTIVE is set', () => {
-    process.env.LEGACY_REPORTS_ACTIVE = 'true'
-    config.legacyReportsActive = true
-    getReportTypes()
-    expect(consoleSpy).toHaveBeenCalledWith('Legacy reports are active in this environment.')
-  })
-
-  test('logs "Legacy reports are not active in this environment." when LEGACY_REPORTS_ACTIVE is not set', () => {
-    delete process.env.LEGACY_REPORTS_ACTIVE
-    config.legacyReportsActive = false
-    getReportTypes()
-    expect(consoleSpy).toHaveBeenCalledWith('Legacy reports are not active in this environment.')
   })
 })
