c1151773023b2d57715f2b2b446166d6466f0d7c
Oliver Lewington <oliver.lewington@atos.net>
2026-03-26 13:46:12 +0000

    Adding toggle to disable legacy reports

--- app/plugins/router.js
+++ app/plugins/router.js
@@ -1,4 +1,6 @@
-const routes = [].concat(
+const config = require('../config')
+
+let routes = [].concat(
   require('../routes/healthy'),
   require('../routes/healthz'),
   require('../routes/static'),
@@ -9,25 +11,27 @@ const routes = [].concat(
   require('../routes/holds'),
   require('../routes/monitoring'),
   require('../routes/payment-requests'),
-  require('../routes/report-list/report'),
   require('../routes/dev-auth'),
   require('../routes/view-processed-payment-requests'),
   require('../routes/closures'),
   require('../routes/view-closures'),
-  require('../routes/report-list'),
   require('../routes/loading'),
+  require('../routes/report-list'),
+  require('../routes/report-list/report'),
   require('../routes/report-list/ap-ar-report'),
   require('../routes/report-list/report-generation'),
   require('../routes/report-list/request-editor-report'),
   require('../routes/report-list/payment-requests-report-v2'),
-  require('../routes/report-list/claim-level-report'),
-  require('../routes/report-list/transaction-summary-report'),
   require('../routes/report-list/status-report'),
   require('../routes/manual-payments'),
   require('../routes/alerts'),
   require('../routes/metrics'),
   require('../routes/download-statements')
 )
+
+if (config.legacyReportsEnabled) {
+  routes = routes.concat(require('../routes/report-list/claim-level-report'), require('../routes/report-list/transaction-summary-report'))
+}
 
 module.exports = {
   plugin: {
