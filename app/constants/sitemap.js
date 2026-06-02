6268a8ebce8de7594df7f2941dd7195827788174
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2026-06-02 12:23:41 +0100

    Homepage redesign (#218)

--- app/constants/sitemap.js
+++ app/constants/sitemap.js
@@ -1,13 +1,15 @@
+const permissions = require('../auth/permissions')
+
 const HOME = { href: '/', text: 'Home' }
 
 const PAYMENT_HOLDS_LINKS = [
-  { href: '/payment-holds', text: 'Manage holds' },
+  { href: '/payment-holds', text: 'Manage holds', homeAuth: [permissions.applicationAdmin, permissions.holdAdmin] },
   { href: '/add-payment-hold', text: 'Add payment hold' },
   { href: '/payment-holds/bulk', text: 'Bulk payment holds' },
 ]
 
 const FINANCE_REPORTS_LINKS = [
-  { href: '/report-list', text: 'Reports' },
+  { href: '/report-list', text: 'Reports', homeAuth: [permissions.applicationAdmin, permissions.holdAdmin, permissions.schemeAdmin, permissions.dataView, permissions.statusReportsDelinked, permissions.statusReportSfi23] },
   { href: '/report-list/payment-requests-v2', text: 'Payment request statuses report' },
   { href: '/report-list/ap-ar-report', text: 'AP/AR listing report' },
   { href: '/report-list/request-editor-report', text: 'Request editor report' },
@@ -15,36 +17,36 @@ const FINANCE_REPORTS_LINKS = [
 ]
 
 const PAYMENT_ALERTS_LINKS = [
-  { href: '/alerts', text: 'Alerts' },
+  { href: '/alerts', text: 'Alerts', homeAuth: [permissions.applicationAdmin, permissions.alertAdmin] },
   { href: '/alerts/information', text: 'Alerts information' },
   { href: '/alerts/update', text: 'Add new alert recipient' },
 ]
 
 const AGREEMENT_CLOSURES_LINKS = [
-  { href: '/closure', text: 'Manage closures' },
+  { href: '/closure', text: 'Manage closures', homeAuth: [permissions.applicationAdmin, permissions.closureAdmin] },
   { href: '/closure/add', text: 'Agreement closure' },
   { href: '/closure/bulk', text: 'Bulk agreement closure' },
 ]
 
 const MONITORING_LINKS = [
-  { href: '/monitoring', text: 'Monitoring' },
+  { href: '/monitoring', text: 'Monitoring', homeAuth: [permissions.applicationAdmin, permissions.holdAdmin, permissions.schemeAdmin, permissions.dataView] },
   { href: '/monitoring/schemes', text: 'Schemes' },
 ]
 
 const MANUAL_PAYMENTS_LINKS = [
-  { href: '/manual-payments', text: 'Manual payment upload' }
+  { href: '/manual-payments', text: 'Manual payment upload', homeAuth: [permissions.applicationAdmin, permissions.manualPaymentsAdmin] }
 ]
 
 const RESET_PAYMENT_REQUEST_LINKS = [
-  { href: '/payment-request/reset', text: 'Reset payment request' }
+  { href: '/payment-request/reset', text: 'Reset payment request', homeAuth: [permissions.applicationAdmin, permissions.schemeAdmin] }
 ]
 
 const METRICS_LINKS = [
-  { href: '/metrics', text: 'Management information' }
+  { href: '/metrics', text: 'Management information', homeAuth: [permissions.applicationAdmin, permissions.schemeAdmin] }
 ]
 
 const DOWNLOAD_STATEMENTS_LINKS = [
-  { href: '/download-statements', text: 'Download statements' }
+  { href: '/download-statements', text: 'Download statements', homeAuth: [permissions.applicationAdmin] }
 ]
 
 const HELP_LINKS = [
@@ -55,14 +57,14 @@ const HELP_LINKS = [
 
 module.exports = [
   { title: '', links: [HOME] },
-  { title: 'Payment holds', links: PAYMENT_HOLDS_LINKS },
-  { title: 'Finance reports', links: FINANCE_REPORTS_LINKS },
-  { title: 'Payment alerts', links: PAYMENT_ALERTS_LINKS },
-  { title: 'Agreement closures', links: AGREEMENT_CLOSURES_LINKS },
-  { title: 'Payment event monitoring', links: MONITORING_LINKS },
-  { title: 'Manual payment upload', links: MANUAL_PAYMENTS_LINKS },
-  { title: 'Reset payment requests', links: RESET_PAYMENT_REQUEST_LINKS },
-  { title: 'Management information', links: METRICS_LINKS },
-  { title: 'Download statements', links: DOWNLOAD_STATEMENTS_LINKS },
-  { title: 'Help', links: HELP_LINKS },
+  { title: 'Reports', description: 'Generate and download reports', links: FINANCE_REPORTS_LINKS },
+  { title: 'Payment events', description: 'View payment and events and requests', links: MONITORING_LINKS },
+  { title: 'Payment holds', description: 'View, add or remove payment holds', links: PAYMENT_HOLDS_LINKS },
+  { title: 'Manual payments', description: 'Manually upload payment files', links: MANUAL_PAYMENTS_LINKS },
+  { title: 'Agreement closures', description: 'Update, add and remove payment suppressions', links: AGREEMENT_CLOSURES_LINKS },
+  { title: 'Email alerts', description: 'Manage where payment alerts are sent by the scheme or recipient', links: PAYMENT_ALERTS_LINKS },
+  { title: 'Statements', description: 'Download payment statements and view payment status reports', links: DOWNLOAD_STATEMENTS_LINKS },
+  { title: 'Metrics', description: 'View payment and document metrics by scheme', links: METRICS_LINKS },
+  { title: 'Reset payment requests', description: 'Manually reset payment requests', links: RESET_PAYMENT_REQUEST_LINKS },
+  { title: 'Help', links: HELP_LINKS }
 ]
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,7 @@
 const config = require('../config')
 const { applicationAdmin, holdAdmin, schemeAdmin, dataView, closureAdmin, statusReportSfi23, statusReportsDelinked, manualPaymentsAdmin, alertAdmin } = require('../auth/permissions')
 const { getReportTypes } = require('../helpers/get-report-types')
+const sitemap = require('../constants/sitemap')
 
 const AUTH_SCOPE = { scope: [applicationAdmin, holdAdmin, schemeAdmin, dataView, closureAdmin, statusReportSfi23, statusReportsDelinked, manualPaymentsAdmin, alertAdmin] }
 
@@ -9,11 +10,25 @@ module.exports = {
   path: '/',
   options: {
     auth: AUTH_SCOPE,
-    handler: async (_request, h) => {
+    handler: async (request, h) => {
       const reportTypes = Object.keys(getReportTypes())
+      const cards = [...sitemap]
+      cards.shift()
+      cards.pop()
+      for (const card of cards) {
+        if (Array.isArray(card.links)) {
+          card.links = card.links.filter(link => link.homeAuth)
+          card.hasAccess = card.links.some(link => {
+            return link.homeAuth.some(authFlag => request.auth.credentials.scope.includes(authFlag))
+          })
+        }
+      }
+
+      const accessibleCards = cards.filter(card => card.hasAccess)
       return h.view('home', {
         reportTypes,
-        manualPaymentsActive: config.manualPaymentsActive
+        manualPaymentsActive: config.manualPaymentsActive,
+        cards: accessibleCards
       })
     }
   }
