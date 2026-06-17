c1c4c93234cb53b0667d1141286cd231da5f5289
Sam Plackett <samuel.plackett@eviden.com>
2026-06-17 16:01:02 +0100

    standardise AHWR scheme name

--- app/helpers/get-schemes.js
+++ app/helpers/get-schemes.js
@@ -7,6 +7,9 @@ const getSchemes = async () => {
     if (scheme.name === 'SFI') {
       scheme.name = 'SFI22'
     }
+    if (scheme.name === 'Vet Visits') {
+      scheme.name = 'Annual Health and Welfare Review'
+    }
   })
   schemesPayload.sort((a, b) => a.name.localeCompare(b.name))
   return schemesPayload
