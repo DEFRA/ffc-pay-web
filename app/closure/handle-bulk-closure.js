e8a1759653b29aae1ddd01eb6e0fd43631f27d46
Sam Plackett <samuel.plackett@eviden.com>
2026-07-08 17:38:59 +0100

    refactor for sonar

--- app/closure/handle-bulk-closure.js
+++ app/closure/handle-bulk-closure.js
@@ -5,14 +5,24 @@ const { handleBulkClosureError } = require('./handle-bulk-closure-error')
 const { BULK, MANAGE } = require('../constants/closures-routes')
 const { SFI } = require('../constants/source-systems')
 
-const checkFileStructure = (file, h, request) => {
-  if (!file || typeof file.path !== 'string') {
-    return handleBulkClosureError(h, 'Invalid file structure or missing file path.', request.payload?.crumb ?? request.state.crumb)
+const readAndCheckFile = (path, request, h) => {
+  let data
+  try {
+    data = fs.readFileSync(path, 'utf8')
+  } catch (err) {
+    console.error('Error reading file:', err)
+    return handleBulkClosureError(h, 'An error occurred whilst reading the file.', request.payload?.crumb ?? request.state.crumb)
   }
+
+  if (!data) {
+    return handleBulkClosureError(h, 'File is empty or could not be read.', request.payload?.crumb ?? request.state.crumb)
+  }
+
+  return data
 }
 
 const handleSFI22Closures = async (data) => {
-  const sfi22Data = data.filter(item => item.source === SFI)
+  const sfi22Data = data.filter(item => item.sourceSystem === SFI)
   if (sfi22Data.length > 0) {
     for (const item of sfi22Data) {
       delete item.sourceSystem
@@ -25,19 +35,15 @@ const handleBulkClosure = async (request, h) => {
   const file = request.payload.file
 
   // Validate file structure
-  checkFileStructure(file, h, request)
-
-  let data
-  try {
-    data = fs.readFileSync(file.path, 'utf8')
-  } catch (err) {
-    console.error('Error reading file:', err)
-    return handleBulkClosureError(h, 'An error occurred whilst reading the file.', request.payload?.crumb ?? request.state.crumb)
+  if (!file || typeof file.path !== 'string') {
+    return handleBulkClosureError(h, 'Invalid file structure or missing file path.', request.payload?.crumb ?? request.state.crumb)
   }
 
-  if (!data) {
-    return handleBulkClosureError(h, 'File is empty or could not be read.', request.payload?.crumb ?? request.state.crumb)
+  const dataOrErrorResponse = readAndCheckFile(file.path, request, h)
+  if (typeof dataOrErrorResponse !== 'string') {
+    return dataOrErrorResponse
   }
+  const data = dataOrErrorResponse
 
   const { uploadData, errors } = await processClosureData(data)
   if (errors) {
