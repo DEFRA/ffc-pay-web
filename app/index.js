4809b73442dd776d9a2aa0e5619a3138eaa87bdd
awishart1989 <alistair.wishart@atos.net>
2026-08-06 11:17:21 +0100

    Startup refactor

--- app/index.js
+++ app/index.js
@@ -1,15 +1,10 @@
 require('log-timestamp')
 require('./insights').setup()
 
-const createServer = require('./server')
+const startServer = require('./start-server')
 
 const startApp = async () => {
-  createServer()
-    .then(server => server.start())
-    .catch(err => {
-      console.log(err)
-      process.exit(1)
-    })
+  startServer()
 }
 
 (async () => {
---
+++ app/start-server.js
@@ -0,0 +1,9 @@
+const createServer = require('./server')
+
+const startServer = async () => {
+    const server = createServer().then(server => server.start())
+
+    return server
+}
+
+module.exports = startServer
