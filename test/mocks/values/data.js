658d1faccc3d0637d9ec58971d546ca90dd746e7
awishart1989 <alistair.wishart@atos.net>
2026-08-12 16:00:08 +0100

    Events

--- test/mocks/values/data.js
+++ test/mocks/values/data.js
@@ -1,5 +1,17 @@
 module.exports = {
-  DATA: {
-    payments: 'data'
-  }
+  DATA: [
+    {
+      frn: '123456',
+      scheme: 'SFI',
+      agreementNumber: 'AG123',
+      paymentRequestNumber: 1,
+      originalValueText: '£100.00',
+      lastUpdated: '01/01/2026',
+      correlationId: 'abc123',
+      status: {
+        state: 'Completed',
+        detail: 'Completed'
+      }
+    }
+  ]
 }
