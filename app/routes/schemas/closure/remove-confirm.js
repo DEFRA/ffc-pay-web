79cb9768f145a0dd477194d629e4563986bf04f9
awishart1989 <alistair.wishart@atos.net>
2026-08-25 15:44:25 +0100

    Remove Confirm fix

--- app/routes/schemas/closure/remove-confirm.js
+++ app/routes/schemas/closure/remove-confirm.js
@@ -1,8 +1,37 @@
 const Joi = require('joi').extend(require('@joi/date'))
 
 module.exports = Joi.object({
-  retentionDataId: Joi.number().integer().required(),
-  frn: Joi.number().required(),
-  agreementNumber: Joi.string().required(),
-  schemeName: Joi.string().required()
+  retentionDataId: Joi.number()
+    .integer()
+    .required(),
+
+  frn: Joi.number()
+    .required(),
+
+  agreementNumber: Joi.string()
+    .required(),
+
+  schemeName: Joi.string()
+    .required(),
+
+  page: Joi.number()
+    .integer()
+    .min(1)
+    .optional(),
+
+  perPage: Joi.number()
+    .integer()
+    .min(1)
+    .optional(),
+
+  frnAgreement: Joi.string()
+    .allow('')
+    .optional(),
+
+  schemeId: Joi.alternatives()
+    .try(
+      Joi.number().integer(),
+      Joi.string().allow('')
+    )
+    .optional()
 })
