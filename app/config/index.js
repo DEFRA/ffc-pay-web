3ab4f72960e7ad62baa78a238300abe09b7d2a66
awishart1989 <alistair.wishart@atos.net>
2026-09-25 12:43:06 +0100

    lint

--- app/config/index.js
+++ app/config/index.js
@@ -105,9 +105,9 @@ value.cache = cacheConfig
 
 value.cache.catboxOptions = value.useRedis
   ? {
-    ...cacheConfig.catboxOptions,
-    tls: value.isDev ? undefined : { minVersion: 'TLSv1.2' }
-  }
+      ...cacheConfig.catboxOptions,
+      tls: value.isDev ? undefined : { minVersion: 'TLSv1.2' }
+    }
   : {}
 value.cache.catbox = value.useRedis ? require('@hapi/catbox-redis') : require('@hapi/catbox-memory')
 
